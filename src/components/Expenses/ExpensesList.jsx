999c3127532ef84f413a93a886c1f1bd6387aba9
Szpytma <szpytmapawel@hotmail.com>
2023-05-15 12:34:43 +0100

    reorganising compnenets

--- src/components/Expenses/ExpensesList.jsx
+++ src/components/Expenses/ExpensesList.jsx
@@ -1,6 +1,6 @@
 import "./ExpensesList.css";
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 function ExpenseList({ expenses }) {
   return (
