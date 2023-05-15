720feebcdbb662ed9f187aaeead97ef29e009d7d
Szpytma <szpytmapawel@hotmail.com>
2023-05-15 11:22:26 +0100

    expenses tracker app initalized with hardocoed data

---
+++ src/components/ExpensesList.jsx
@@ -0,0 +1,22 @@
+import "./ExpensesList.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+
+function ExpenseList({ expenses }) {
+  return (
+    <Card className="expenses">
+      {expenses.map((item) => {
+        return (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        );
+      })}
+    </Card>
+  );
+}
+
+export default ExpenseList;
